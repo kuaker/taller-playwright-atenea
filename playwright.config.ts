086de5239bbdd2dbc14e06042b060300080b911b
kuaker <sbumission@gmail.com>
2025-08-20 19:49:59 -0300

    add-dashboard-page

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,6 +35,11 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'setup',
+      testMatch: /.*\.setup\.ts/,
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
